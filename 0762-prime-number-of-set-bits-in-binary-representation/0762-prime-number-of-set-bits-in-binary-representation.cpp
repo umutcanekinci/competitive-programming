072906c92f581b272637f6d073bcade07169aa5d
Umutcan Ekinci <46852601+umutcanekinci@users.noreply.github.com>
2026-02-21 18:16:41 +0300

    Time: 567 ms (5%), Space: 7.9 MB (88.15%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int countPrimeSetBits(int left, int right) {
-        int count = 0;
-        // Gemini results!!! (only for testing)
-        // 20'ye kadar olan asal sayıların bitmask hali:
-        // (1 << 2) | (1 << 3) | (1 << 5) | ... şeklinde
-        // Bu sayı ikilik sistemde asalların olduğu pozisyonlarda 1 içerir.
-        unsigned int primeMask = 0b10100010100010101100; 
+        int sum = 0;
+        for (int i=left; i<=right; i++) {
+            cout << number_of_bits(i);
+            sum += check(number_of_bits(i));
+        }
+        return sum;
+    }
 
-        for (int i = left; i <= right; ++i) {
-            // 1. Bit sayısını tek seferde hesapla
-            int bits = __builtin_popcount(i);
-            
-            // 2. Bu sayı asal mı? Mask üzerinden kontrol et (O(1))
-            if ((primeMask >> bits) & 1) {
-                count++;
+    bool check(int i) {
+        if (i == 0 || i == 1) {
+            return 0;
+        }
+
+        for(int j=2; j<i; j++) {
+            if (i % j == 0) {
+                return 0;
             }
         }
-        
-        return count;
+        return 1;
+    }
+
+    int number_of_bits(int i) {
+        int sum = 0;
+        while(i != 0) {
+            sum += i % 2;
+            i /= 2; 
+        }
+        return sum;
     }
 };
