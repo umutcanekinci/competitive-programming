ae3a744a1919c4a6d9759d14ec30392df0790aaf
Umutcan Ekinci <46852601+umutcanekinci@users.noreply.github.com>
2026-02-21 18:05:49 +0300

    Time: 11 ms (33.71%), Space: 8 MB (35.12%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -3,22 +3,31 @@ public:
     int countPrimeSetBits(int left, int right) {
         int sum = 0;
         for (int i=left; i<=right; i++) {
-            cout << number_of_bits(i);
-            sum += check(number_of_bits(i));
+            sum += isPrime(number_of_bits(i));
         }
         return sum;
     }
 
-    bool check(int i) {
-        if (i == 0 || i == 1) {
+    bool isPrime(int n) {
+  
+        // Check if n is 1 or 0
+        if (n <= 1)
             return 0;
-        }
 
-        for(int j=2; j<i; j++) {
-            if (i % j == 0) {
+        // Check if n is 2 or 3
+        if (n == 2 || n == 3)
+            return 1;
+
+        // Check whether n is divisible by 2 or 3
+        if (n % 2 == 0 || n % 3 == 0)
+            return 0;
+        
+        // Check from 5 to square root of n
+        // Iterate i by (i+6)
+        for (int i = 5; i *i<=n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
                 return 0;
-            }
-        }
+
         return 1;
     }
 
