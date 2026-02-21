a64dab125c7587f2dd906b745c893fccdc650b21
Umutcan Ekinci <46852601+umutcanekinci@users.noreply.github.com>
2026-02-21 18:06:18 +0300

    Time: 0 ms (100%), Space: 8 MB (61.81%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
     int countPrimeSetBits(int left, int right) {
-        int sum = 0;
-        for (int i=left; i<=right; i++) {
-            sum += isPrime(number_of_bits(i));
-        }
-        return sum;
-    }
-
-    bool isPrime(int n) {
-  
-        // Check if n is 1 or 0
-        if (n <= 1)
-            return 0;
-
-        // Check if n is 2 or 3
-        if (n == 2 || n == 3)
-            return 1;
-
-        // Check whether n is divisible by 2 or 3
-        if (n % 2 == 0 || n % 3 == 0)
-            return 0;
+        int count = 0;
         
-        // Check from 5 to square root of n
-        // Iterate i by (i+6)
-        for (int i = 5; i *i<=n; i = i + 6)
-            if (n % i == 0 || n % (i + 2) == 0)
-                return 0;
+        // 20'ye kadar olan asal sayıların bitmask hali:
+        // (1 << 2) | (1 << 3) | (1 << 5) | ... şeklinde
+        // Bu sayı ikilik sistemde asalların olduğu pozisyonlarda 1 içerir.
+        unsigned int primeMask = 0b10100010100010101100; 
 
-        return 1;
-    }
-
-    int number_of_bits(int i) {
-        int sum = 0;
-        while(i != 0) {
-            sum += i % 2;
-            i /= 2; 
+        for (int i = left; i <= right; ++i) {
+            // 1. Bit sayısını tek seferde hesapla
+            int bits = __builtin_popcount(i);
+            
+            // 2. Bu sayı asal mı? Mask üzerinden kontrol et (O(1))
+            if ((primeMask >> bits) & 1) {
+                count++;
+            }
         }
-        return sum;
+        
+        return count;
     }
 };
