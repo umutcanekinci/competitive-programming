073da2260993bbf393adc4992be06cf6663d3d3b
Umutcan Ekinci <46852601+umutcanekinci@users.noreply.github.com>
2026-02-26 14:36:46 +0300

    Time: 51 ms (5.18%), Space: 9.6 MB (13.47%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,92 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        int index = 0;
+        cout << "Number: " << s << endl;
+        while (!isOne(s)) {
+            if (!isEven(s)) {
+                addOne(s);
+                cout << "Plus one: " << s << endl;
+            } else {
+                divideByTwo(s);
+                cout << "Divide: " << s << endl;
+            }
+            index++;
+        }
+        return index;
+    }
+
+    bool isOne(string &s) {
+        for (int i=0; i<s.size()-1; i++) {
+            if (s[i] == '1') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool isEven(string &s) {
+        return s[s.size()-1] == '0';
+    }
+
+    void addOne(string &s) {
+        int size = s.size();
+        for(int i=size-1; i>=0; i--) {
+            char ch = s[i];
+            if (ch == '0') {
+                s[i] = '1';
+                return;
+            } else {
+                s[i] = '0';
+            }
+        }
+        s = '1' + s;
+    }
+
+    void divideByTwo(string &s) {
+        // This can't be used with strings!
+        // s = s >> 1;
+        
+        int l = s.size();
+        int starting = l-2;
+        for(int i=starting; i>=0; i--) {
+            s[i+1] = s[i];
+        }
+        s[0] = '0';
+    }
+
+    // int numSteps(string s) {
+    //     return numSteps(binaryToDecimal(s));
+    // }
+
+    // long long binaryToDecimal(string s) {
+    //     long long result = 0;
+
+    //     int l = s.size();
+    //     for (int i=l-1; i>=0; i--) {
+    //         char ch = s[i];
+    //         result += (ch == '0') ? 0 : pow(2, l-1-i);
+    //     }
+    //     return result;
+    // }
+
+    // long long pow(int base, int power) {
+    //     if (power <= 0) {
+    //         return 1;
+    //     }
+    //     return pow(base, power - 1) * base;
+    // }
+
+    // int numSteps(int n) {
+    //     int i = 0;
+    //     while (n != 1) {
+    //         if (n % 2) {
+    //             n++;
+    //         } else {
+    //             n /= 2;
+    //         }
+    //         i++;
+    //     }
+    //     return i;
+    // }
+};
