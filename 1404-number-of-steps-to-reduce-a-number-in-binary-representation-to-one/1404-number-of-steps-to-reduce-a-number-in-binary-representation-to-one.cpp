19efa82d47650b810e56359103a84b44bd0af977
Umutcan Ekinci <46852601+umutcanekinci@users.noreply.github.com>
2026-02-26 14:52:45 +0300

    Time: 0 ms (100%), Space: 9.6 MB (13.47%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int numSteps(string s) {
         int index = 0;
-        int size = s.size();
-        while (isOne(s, size)) {
-            if (isEven(s, size)) {
-                divideByTwo(s, size);
+        while (isOne(s)) {
+            if (isEven(s)) {
+                divideByTwo(s);
             } else {
-                size += addOne(s, size);
+                addOne(s);
             }
             index++;
         }
         return index;
     }
 
-    bool isOne(string &s, int size) {
-        for (int i=0; i<size-1; i++) {
+    bool isOne(string &s) {
+        for (int i=0; i<s.size()-1; i++) {
             if (s[i] == '1') {
                 return true;
             }
@@ -23,27 +22,65 @@ public:
         return false;
     }
 
-    bool isEven(string &s, int size) {
-        return s[size-1] == '0';
+    bool isEven(string &s) {
+        return s[s.size()-1] == '0';
     }
 
-    bool addOne(string &s, int size) {
+    void addOne(string &s) {
+        int size = s.size();
         for(int i=size-1; i>=0; i--) {
             char ch = s[i];
             if (ch == '0') {
                 s[i] = '1';
-                return 0;
+                return;
             }
             s[i] = '0';
+            
         }
         s = '1' + s;
-        return 1;
     }
 
-    void divideByTwo(string &s, int size) {
-        for(int i=size-2; i>=0; i--) {
+    void divideByTwo(string &s) {
+        int l = s.size();
+        int starting = l-2;
+        for(int i=starting; i>=0; i--) {
             s[i+1] = s[i];
         }
         s[0] = '0';
     }
+
+    // int numSteps(string s) {
+    //     return numSteps(binaryToDecimal(s));
+    // }
+
+    // long long binaryToDecimal(string s) {
+    //     long long result = 0;
+
+    //     int l = s.size();
+    //     for (int i=l-1; i>=0; i--) {
+    //         char ch = s[i];
+    //         result += (ch == '0') ? 0 : pow(2, l-1-i);
+    //     }
+    //     return result;
+    // }
+
+    // long long pow(int base, int power) {
+    //     if (power <= 0) {
+    //         return 1;
+    //     }
+    //     return pow(base, power - 1) * base;
+    // }
+
+    // int numSteps(int n) {
+    //     int i = 0;
+    //     while (n != 1) {
+    //         if (n % 2) {
+    //             n++;
+    //         } else {
+    //             n /= 2;
+    //         }
+    //         i++;
+    //     }
+    //     return i;
+    // }
 };
